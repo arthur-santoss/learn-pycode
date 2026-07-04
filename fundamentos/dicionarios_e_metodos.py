91c1ed292dc0d45834c7cd34fe0e0ac6d1e34b77
Arthur Santos <arthursantos242568@gmail.com>
2026-07-04 11:20:04 -0300

    dicionarios e metodos

---
+++ fundamentos/dicionarios_e_metodos.py
@@ -0,0 +1,13 @@
+pessoa = {
+    "nome": "arthur",
+    "idade": 24,
+    "cidade": "São Paulo",
+    "interesses": ["Muay thai", "programação", "informática"],
+    "pet": { 
+        "nome": "Sombra",  
+            "tipo": "gato",
+            "idade": "5 meses",
+            "peso": 2.5 
+        
+    }
+}

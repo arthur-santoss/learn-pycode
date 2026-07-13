d2bb0f3bad337dd03e66c0cc0c1fd404fcd8f554
Arthur Santos <arthursantos242568@gmail.com>
2026-07-13 16:12:59 -0300

    criando view teste

--- projetos/carros/cars/views.py
+++ projetos/carros/cars/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cars_view(request):
-    return HttpResponse('Meus carros')
+    html = '''
+    <html>
+        <head>
+            <title>Meus carros</title>
+        </head>
+        <body>
+            <H1>Carros PyCode</H1>
+            <p>Só carro top</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+

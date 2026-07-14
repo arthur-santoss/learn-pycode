7753c6a38076525e0b092d5da66c9447654c59ad
Arthur Santos <arthursantos242568@gmail.com>
2026-07-14 14:41:46 -0300

    recuperando valor do GET e tratando pesquisas via URL

--- projetos/carros/cars/views.py
+++ projetos/carros/cars/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()    
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search: 
+        cars = Car.objects.filter(model__icontains=search)
 
     return render(
         request,

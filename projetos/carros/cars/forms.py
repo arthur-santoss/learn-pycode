37f4b028bd96ac76b3d5cb4d1406acdab823e75f
Arthur Santos <arthursantos242568@gmail.com>
2026-07-15 23:36:05 -0300

    validacoes de formulario valido

--- projetos/carros/cars/forms.py
+++ projetos/carros/cars/forms.py
@@ -3,6 +3,20 @@ from cars.models import Brand, Car
 
 
 class CarModelForm(forms.ModelForm):
+    photo = forms.ImageField(required=True)
     class Meta:
         model = Car
         fields = '__all__'
+    
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value is not None and value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year is not None and factory_year < 1975:
+            self.add_error('factory_year', 'Ano mínimo a partir de 1975')
+        return factory_year
+

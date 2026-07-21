27b45947b183a6973a7fc467af71810e4feb4eab
Arthur Santos <arthursantos242568@gmail.com>
2026-07-21 16:12:46 -0300

    Utilizando CreateView (Generic Views)

--- projetos/carros/cars/views.py
+++ projetos/carros/cars/views.py
@@ -1,22 +1,7 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
-class CarsView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-
-        if search: 
-            cars = Car.objects.filter(model__icontains=search)
-
-        return render(
-            request,
-            'cars.html',
-            {'cars':cars}
-        )
     
 class CarsListView(ListView):
     model = Car
@@ -29,15 +14,9 @@ class CarsListView(ListView):
         if search: 
             cars = cars.filter(model__icontains=search)
         return cars
-        
-class NewCarView(View):
-    def get(self,request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
     
-    def post(self,request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')            
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars'

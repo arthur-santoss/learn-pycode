75dc74d07714f5cbe82fb3b01f684f35c71a71d5
Arthur Santos <arthursantos242568@gmail.com>
2026-07-30 09:19:56 -0300

    criado modelo de inventario no models.py

--- projetos/carros/cars/apps.py
+++ projetos/carros/cars/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class CarsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoFields'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'cars'
 
     def ready(self):
